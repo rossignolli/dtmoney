6c1aa57d5da901e451644ec99a347c85d0660a02
Vitor Vigarani Rossignolli <vitorrossignolli@gmail.com>
2021-03-19 19:14:23 -0300

    dt-money

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import { GlobalStyle } from './styles/global'
 
-function App() {
+
+const Title = styled.h1`
+font-size: 64px;
+color: #8525e6;
+`
+
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title >Hello world</Title>
+      <GlobalStyle/>
     </div>
   );
 }
