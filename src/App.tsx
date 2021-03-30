a0557aeeec19a642f081d46084b4f097e57410e5
Vitor Vigarani Rossignolli <vitorrossignolli@gmail.com>
2021-03-29 23:38:35 -0300

    Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import styled from 'styled-components'
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global'
 
 
-const Title = styled.h1`
-font-size: 64px;
-color: #8525e6;
-`
-
 export function App() {
   return (
-    <div className="App">
-      <Title >Hello world</Title>
+    <>
+      <Header/>
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
 
