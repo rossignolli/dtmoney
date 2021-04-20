641d9adf913b5708f52936e8de898b1cee052501
Vitor Vigarani Rossignolli <vitorrossignolli@gmail.com>
2021-04-20 16:44:02 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global'
 import Modal from 'react-modal'
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { NewTransactionModal } from './components/NewTransactionsModal';
 import { TransactionsProvider } from './hooks/useTransactions';
 
